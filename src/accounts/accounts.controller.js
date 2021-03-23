f5920d54ba94c59be42aba359ec101534cd3f0ce
unknown <daniel.camposdev@gmail.com>
2021-03-23 10:22:28 -0400

    Commit initial

---
+++ src/accounts/accounts.controller.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = express.Router();
+const Joi = require("joi");
+const validateRequest = require("../_middleware/validate-request");
+const authorize = require("../_middleware/authorize");
+const Role = require("../_helpers/role");
+const accountService = require("./account.service");
+
+module.exports = router;
+
+function authenticateSchema(req, res, next) {
+  const schema = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  });
+  validateRequest(req, next, schema);
+}
+
+function authenticate(req, res, next) {
+  const { email, password } = req.body;
+  const ipAddress = req.ip;
+
+  accountService
+    .authenticate({ email, password, ipAddress })
+    .then(({ refreshToken, ...account }) => {
+      setTokenCookie(res, refreshToken);
+      res.json(account);
+    })
+    .catch(next);
+}
+
+function refreshToken(req, res, next) {
+  const token = req.cookies.refreshToken;
+  const ipAddress = req.ip;
+  accountService
+    .refreshToken({ token, ipAddress })
+    .then(({ refreshToken, ...account }) => {
+      setTokenCookie(res, refreshToken);
+      res.json(account);
+    })
+    .catch(next);
+}
+
+function revokeTokenSchema(req, res, next) {
+  const schema = Joi.object({
+    token: Joi.string().empty(""),
+  });
+  validateRequest(req, res, next);
+}
+
+function revokeToken(req, res, next) {
+  const token = req.body.token || req.cookies.refreshToken;
+  const ipAddress = req.ip;
+
+  if (!token) return res.status(400).json({ message: "Token is required" });
+
+  if (!req.user.ownsToken(token) && req.user.role !== Role.Admin) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  accountService
+    .revokeToken({ token, ipAddress })
+    .then(() => res.json({ message: "Token revoked" }))
+    .catch(next);
+}
